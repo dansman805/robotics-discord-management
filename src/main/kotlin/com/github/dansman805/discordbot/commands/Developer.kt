ff119455c25ca3ddc23cadcb77735c258f412cc1
Daniel Goz <dansman805@gmail.com>
2020-04-14 20:54:24 -0400

    Added better error messaging for eval

--- src/main/kotlin/com/github/dansman805/discordbot/commands/Developer.kt
+++ src/main/kotlin/com/github/dansman805/discordbot/commands/Developer.kt
@@ -2,16 +2,13 @@ package com.github.dansman805.discordbot.commands
 
 import com.github.dansman805.discordbot.botConfig
 import com.github.dansman805.discordbot.services.CodeEvalService
-import com.github.dansman805.discordbot.services.jda
 import me.aberrantfox.kjdautils.api.annotation.CommandSet
 import me.aberrantfox.kjdautils.api.annotation.Precondition
 import me.aberrantfox.kjdautils.api.dsl.command.commands
-import me.aberrantfox.kjdautils.internal.arguments.Manual
 import me.aberrantfox.kjdautils.internal.arguments.SentenceArg
 import me.aberrantfox.kjdautils.internal.command.Fail
 import me.aberrantfox.kjdautils.internal.command.Pass
 import me.aberrantfox.kjdautils.internal.command.precondition
-import javax.script.ScriptEngineManager
 
 const val developerCategoryName = "Developer"
 
@@ -36,11 +33,7 @@ fun developerCommands(evalService: CodeEvalService) = commands {
             val rawCode = it.args.first
             val code = rawCode.replace("```.*", "").replace("`", "")
 
-            val result = evalService.runCode(code, it.author.jda)
-
-            if (result != null) {
-                it.respond(result)
-            }
+            it.respond(evalService.runCode(code, it.author.jda) ?: "Done.")
         }
     }
 }
--- src/main/kotlin/com/github/dansman805/discordbot/services/CodeEvalService.kt
+++ src/main/kotlin/com/github/dansman805/discordbot/services/CodeEvalService.kt
@@ -2,8 +2,12 @@ package com.github.dansman805.discordbot.services
 
 import me.aberrantfox.kjdautils.api.annotation.Service
 import de.swirtz.ktsrunner.objectloader.KtsObjectLoader
+import de.swirtz.ktsrunner.objectloader.LoadException
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.entities.Emote
+import java.io.PrintWriter
+import java.io.StringWriter
+import java.lang.Exception
 
 var jda: JDA? = null
 
@@ -21,8 +25,16 @@ class CodeEvalService {
                             "import me.aberrantfox.kjdautils.*\n" +
                             code).toString()
         }
-        catch (e: IllegalArgumentException) {
-            null
+        catch (e: Exception) {
+            when (e) {
+                is IllegalArgumentException -> null
+                is LoadException -> {
+                    val stringWriter = StringWriter()
+                    e.printStackTrace(PrintWriter(stringWriter))
+                    stringWriter.toString().substringAfter("Caused by:")
+                }
+                else -> throw e
+            }
         }
     }
 }
